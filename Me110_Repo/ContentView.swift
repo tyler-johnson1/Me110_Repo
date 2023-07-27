f564a7361fda583953ce35b7426e95865204bbf8
tyler-johnson1 <tylerjohnson92200@berkeley.edu>
2023-07-26 18:05:14 -0700

    Test commit

--- Me110_Repo/ContentView.swift
+++ Me110_Repo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("This is a test of the Me110 Repo")
             .padding()
     }
 }
