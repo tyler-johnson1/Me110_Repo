2c770bef97c0b61fd0195c12cbe778148dc89b33
tyler-johnson1 <tylerjohnson92200@berkeley.edu>
2023-07-26 18:04:32 -0700

    Initial Commit

---
+++ Me110_Repo/Me110_RepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Me110_RepoApp.swift
+//  Me110_Repo
+//
+//  Created by Tyler Johnson on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Me110_RepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Me110_RepoUITests/Me110_RepoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Me110_RepoUITestsLaunchTests.swift
+//  Me110_RepoUITests
+//
+//  Created by Tyler Johnson on 7/26/23.
+//
+
+import XCTest
+
+class Me110_RepoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
